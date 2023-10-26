90f04fb4ffa55d902d013f0f4060f2665dd3decc
unknown <niarafic@gmail.com>
2023-10-26 17:56:33 -0300

    update

--- inicio/models.py
+++ inicio/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Usuario(models.Model):
-    usuario=models.Charfield(max_length=30)
-    descripcion=models.Charfield(max_length=30)
+
     
